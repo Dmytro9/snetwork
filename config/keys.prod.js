6f179bb8d7198a05162d6c1bff2ed7f903394d05
INNOVECS\dmytro.poliuliuk <Dmytro.Poliuliuk@innovecs.com>
2018-08-09 14:18:49 +0300

    Prepared for deply

--- config/keys.prod.js
+++ config/keys.prod.js
@@ -1,5 +1,4 @@
-export default
-  {
-    mongoURI: process.env.MONGO_URI,
-    secretOrKey: process.env.SECRET_OR_KEY,
-  }
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY
+};
