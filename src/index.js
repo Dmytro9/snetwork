de8d17f180f11d5aee1b14189f6f46649a266e3d
INNOVECS\dmytro.poliuliuk <Dmytro.Poliuliuk@innovecs.com>
2018-08-09 13:25:13 +0300

    Prepared for deply

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
 import passport from 'passport'
+import path from 'path'
 
 import users from './routes/api/users'
 import profile from './routes/api/profile'
@@ -32,6 +33,16 @@ pass(passport)
 app.use('/api/users', users)
 app.use('/api/profile', profile)
 app.use('/api/posts', posts)
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+// Set static folder
+  app.use(express.static('../client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+  })
+}
 
 
 const port = process.env.PORT || 1337
