3978ece17b76973ff3acad0dafe50a9ee0076157
INNOVECS\dmytro.poliuliuk <Dmytro.Poliuliuk@innovecs.com>
2018-08-09 15:27:13 +0300

    Prepared for deply

--- config/passport.js
+++ config/passport.js
@@ -1,23 +1,24 @@
-import { Strategy } from 'passport-jwt'
-import { ExtractJwt } from 'passport-jwt'
-import mongoose from 'mongoose'
-import config from '../config/keys'
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
+const keys = require('../config/keys');
 
-const User = mongoose.model('users')
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = keys.secretOrKey;
 
-const opts = {}
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
-opts.secretOrKey = config.secretOrKey
-
-export default passport => {
-  passport.use(new Strategy(opts, (jwt_payload, done) => {
-    User.findById(jwt_payload.id)
-      .then(user => {
-        if (user) {
-          return done(null, user)
-        }
-        return done(null, false)
-      })
-      .catch(err => console.log(err))
-  }))
-}
+module.exports = passport => {
+  passport.use(
+    new JwtStrategy(opts, (jwt_payload, done) => {
+      User.findById(jwt_payload.id)
+        .then(user => {
+          if (user) {
+            return done(null, user);
+          }
+          return done(null, false);
+        })
+        .catch(err => console.log(err));
+    })
+  );
+};
